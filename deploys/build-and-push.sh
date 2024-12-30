2e1a4b72e8449d71b21eb2d1df645d426be6448e
Abdelhakim OUFKIR <abdelhakimoufkir2000@gmail.com>
2024-12-30 17:26:57 +0100

    editing 5

--- deploys/build-and-push.sh
+++ deploys/build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
 
-# Navigate to the deploys folder (optional if already in this folder)
+# Navigate to the deploys folder
 cd "$(dirname "$0")"
 
 # Azure Container Registry (ACR) name
@@ -19,11 +19,16 @@ declare -A services=(
 
 # Build, tag, and push each service
 for service in "${!services[@]}"; do
-  echo "Building and tagging image for $service..."
-  docker build -t "$ACR_NAME/$service:latest" "${services[$service]}"
-  
-  echo "Pushing image for $service to ACR..."
-  docker push "$ACR_NAME/$service:latest"
+  if [ -d "${services[$service]}" ]; then
+    echo "Building and tagging image for $service..."
+    docker build -t "$ACR_NAME/$service:latest" "${services[$service]}"
+    
+    echo "Pushing image for $service to ACR..."
+    docker push "$ACR_NAME/$service:latest"
+  else
+    echo "Error: Directory '${services[$service]}' not found for service '$service'."
+    exit 1
+  fi
 done
 
 echo "All images have been built, tagged with 'latest', and pushed to $ACR_NAME successfully!"
